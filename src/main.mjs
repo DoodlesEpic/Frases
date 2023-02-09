c961289e5444cb1aaf06043f655f2ac209971c75
Doodles <moraes.eduardo@proton.me>
2023-02-09 02:47:21 -0300

    feat: Use Firestore Lite to reduce bundle size
    Reduces total JS bundle size from 271kb to 93kb, and only 20kb gzipped
    which is a huge win.

--- src/main.mjs
+++ src/main.mjs
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc } from "firebase/firestore/lite";
 import "./styles.scss";
 
 // Inicializar Firebase
